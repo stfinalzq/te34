22e1cc486796fb181fe25c0324c3d29bb74b341b
stfinalzq <stfinal635@gmail.com>
2026-08-12 22:33:11 -0300

    Add files via upload

---
+++ _next/static/A8Po4BgjyRbt4xFEuFgpI/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002FlinksMap\u002Fblog\u002Fblog","\u002FratesAndDeadlines\u002FratesAndDeadlines","\u002Fterms\u002Fterms"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
